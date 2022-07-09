d2979be14383f85977a508c02feaeaa231ba90c8
tr00per <kolosovskiy91@gmail.com>
2022-07-09 23:45:15 +0300

    Сформированы все фрагменты и макеты для них

---
+++ app/src/main/java/com/example/braintrainer/presentation/GameFragment.kt
@@ -0,0 +1,18 @@
+package com.example.braintrainer.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.braintrainer.R
+
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/braintrainer/presentation/WelcomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.braintrainer.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.braintrainer.R
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+}
